387de072a8b3dd3427a354728e5d85c53054ee6d
mlartix <74879660+mlartix@users.noreply.github.com>
2022-05-14 23:31:55 -0500

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'TeeHeeLibSettings'
+	SaveManager.Folder = 'Tee_Hee_Settings'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
